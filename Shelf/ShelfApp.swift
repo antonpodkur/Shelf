3f9d4a59377053ac263f60d0a8ca55204b5971b1
anton.podkur <anton.podkur@gmail.com>
2026-04-16 12:33:30 +0300

    Initial Commit

---
+++ Shelf/ShelfApp.swift
@@ -0,0 +1,32 @@
+//
+//  ShelfApp.swift
+//  Shelf
+//
+//  Created by Anton Podkur on 16.04.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ShelfApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ShelfTests/ShelfTests.swift
@@ -0,0 +1,19 @@
+//
+//  ShelfTests.swift
+//  ShelfTests
+//
+//  Created by Anton Podkur on 16.04.2026.
+//
+
+import Testing
+@testable import Shelf
+
+struct ShelfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
